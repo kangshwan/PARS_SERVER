0988df315a2a70f519a37a1f59ae0f49a6009288
kangshwan <rnltls97@gmail.com>
2020-06-20 19:24:53 +0900

    [UPDATE] renew html templates more better

--- pars/dogfeed/urls.py
+++ pars/dogfeed/urls.py
@@ -5,6 +5,7 @@ from django.conf.urls.static import static
 from . import views
 
 urlpatterns = [
+    path('', views.Main_dog.as_view()),
     path('home/', views.Main_dog.as_view(), name='home'),
     path('ourdog/', views.Our_dog.as_view(), name='ourdog'),
     path('viewdog/',views.View_dog.as_view(), name='viewdog')
--- pars/pars/urls.py
+++ pars/pars/urls.py
@@ -17,6 +17,7 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
+    path('',include('dogfeed.urls')),
     path('admin/', admin.site.urls),
     path('api/v1', include('dogfeed.urls')),
     path('dogfeed/', include('dogfeed.urls'),)
